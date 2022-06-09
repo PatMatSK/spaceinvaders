0680bad2008eabc40b19cc0283a10fea9264b141
patrik matula <matulpat@cvut.fit.cz>
2022-06-09 16:53:43 -0400

    new documentation

--- doc/search/all_1.js
+++ doc/search/all_1.js
@@ -1,5 +1,8 @@
 var searchData=
 [
   ['enemy_0',['Enemy',['../classEnemy.html',1,'']]],
-  ['enemyarmy_1',['EnemyArmy',['../classEnemyArmy.html',1,'']]]
+  ['enemyarmy_1',['EnemyArmy',['../classEnemyArmy.html',1,'']]],
+  ['enemybasic_2',['EnemyBasic',['../classEnemyBasic.html',1,'']]],
+  ['enemycrazy_3',['EnemyCrazy',['../classEnemyCrazy.html',1,'']]],
+  ['enemysnake_4',['EnemySnake',['../classEnemySnake.html',1,'']]]
 ];
