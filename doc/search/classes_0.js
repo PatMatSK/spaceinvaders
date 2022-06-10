787a3d6b72042e96c03715cff492718446315546
patrik matula <matulpat@cvut.fit.cz>
2022-06-10 11:51:20 -0400

    documentation 3.0

--- doc/search/classes_0.js
+++ doc/search/classes_0.js
@@ -1,4 +1,11 @@
 var searchData=
 [
-  ['bullet_0',['Bullet',['../classBullet.html',1,'']]]
+  ['bonus_0',['Bonus',['../classBonus.html',1,'']]],
+  ['bonusdeleteobstacle_1',['BonusDeleteObstacle',['../classBonusDeleteObstacle.html',1,'']]],
+  ['bonusrepair_2',['BonusRepair',['../classBonusRepair.html',1,'']]],
+  ['bonusshield_3',['BonusShield',['../classBonusShield.html',1,'']]],
+  ['bonusstop_4',['BonusStop',['../classBonusStop.html',1,'']]],
+  ['bullet_5',['Bullet',['../classBullet.html',1,'']]],
+  ['bulletbasic_6',['BulletBasic',['../classBulletBasic.html',1,'']]],
+  ['bulletrandom_7',['BulletRandom',['../classBulletRandom.html',1,'']]]
 ];
